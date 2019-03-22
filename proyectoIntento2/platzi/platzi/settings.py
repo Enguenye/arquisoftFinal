c11acd0d9c66d5a9b3e5c972242e2834a30cae4e
Enguenye <jd.florezg1@uniandes.edu.co>
2019-03-22 08:42:40 -0500

    db

--- proyectoIntento2/platzi/platzi/settings.py
+++ proyectoIntento2/platzi/platzi/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'platzi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ATpos',
+        'USER': 'ATpos_user',
+        'PASSWORD': 'ATpos_password',
+        'HOST': '3.89.74.59',
+        'PORT': '5432'
+        ),
     }
 }
 
